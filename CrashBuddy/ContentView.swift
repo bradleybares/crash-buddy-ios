ea42f980f8812f79a335f6bc626e11ce851ee464
bradleybares <bradleygbares@gmail.com>
2022-09-12 15:30:32 -0400

    Initial Commit

---
+++ CrashBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrashBuddy
+//
+//  Created by Bradley G Bares on 9/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrashBuddy/CrashBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashBuddyApp.swift
+//  CrashBuddy
+//
+//  Created by Bradley G Bares on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CrashBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
