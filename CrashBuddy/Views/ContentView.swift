03451e2641a1e18bcee9eb0c0703875427f35e7f
bradleybares <bradleygbares@gmail.com>
2022-10-11 12:32:53 -0400

    Add Folder Structure and gitignore

---
+++ CrashBuddy/Views/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  CrashBuddy
+//
+//  Created by Bradley G Bares on 9/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        /*TabView {
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            ActivityView()
+                .tabItem {
+                    Label("Activity", systemImage: "chart.xyaxis.lin")
+                }
+        }*/
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
