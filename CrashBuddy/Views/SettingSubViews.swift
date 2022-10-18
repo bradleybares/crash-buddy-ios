9325a70971947b909cc3c8e962c564537c71c8af
user229036 <user229036@NY545.local>
2022-10-17 22:41:21 -0400

    changing all to nav links and setting up form

--- CrashBuddy/Views/SettingSubViews.swift
+++ CrashBuddy/Views/SettingSubViews.swift
@@ -9,103 +9,152 @@ import SwiftUI
 
 struct EmergencyContactsView: View {
     var body: some View {
-        NavigationView {
-            Text("Emergency Contacts")
+        Text("Emergency Contacts")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     VStack {
                         Text("Emergency Contacts").font(.headline)
-                        }
                     }
                 }
-        }
+            }
     }
 }
 
 struct DebugView: View {
-
+    
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     @GestureState private var dragOffset = CGSize.zero
-
+    @State private var debugOn = true
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Toggle(isOn: .constant(false),
-                            label: {Text("Debug")
-                            })
+                    Toggle(isOn: $debugOn,
+                           label: {Text("Debug")
+                    })
                 }
-
+                
                 //TODO change from toggle
                 // how exactly do we determine whether this is on or not
                 // Does this display only if debug mode is on? or always is it always on display
                 Section {
-                    Toggle(isOn: .constant(false),
-                            label: {Text("Sensor Status")
-                            })
-
-                    Toggle(isOn: .constant(false),
-                            label: {Text("Memory Status")
-                            })
-
-                    Toggle(isOn: .constant(false),
-                            label: {Text("Power Status")
-                            })
+                    Toggle(isOn: $debugOn,
+                           label: {Text("Sensor Status")
+                    })
+                    
+                    Toggle(isOn: $debugOn,
+                           label: {Text("Memory Status")
+                    })
+                    
+                    Toggle(isOn: $debugOn,
+                           label: {Text("Power Status")
+                    })
                 }
             }
         }
         .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: Button(action : {
-                self.mode.wrappedValue.dismiss()
-            }){
-                Image(systemName: "arrow.left")
-            })
         .toolbar {
             ToolbarItem(placement: .principal) {
                 VStack {
-                    Text("Crash Sensitivity").font(.headline)
-                    }
+                    Text("Debug").font(.headline)
                 }
             }
+        }
     }
 }
 
 struct CrashSensitivityView: View {
     var body: some View {
-        NavigationView {
         
-            Text("Crash Sensitivity")
+        Text("Crash Sensitivity")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     VStack {
                         Text("Crash Sensitivity").font(.headline)
-                        }
                     }
                 }
-        }
-  
+            }
     }
 }
 
 struct HardwareConnectivityView: View {
     var body: some View {
-        NavigationView {
-            
-            Text("Hardware Connectivity")
+        
+        Text("Hardware Connectivity")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     VStack {
                         Text("Hardware Connectivity").font(.headline)
-                        }
                     }
                 }
-        }
+            }
     }
 }
 
+struct SportsView: View {
+    
+    @State private var sport: Sport = Sport(name: "Skiing")
+    @State private var crash_sensitivity: Int = 100
+    @State private var showingAlert = false
+    @State private var addedSport = ""
+    
+    @State var sports = [
+        Sport(name: "Skiing"),
+        Sport(name: "Biking"),
+        Sport(name: "Snowboarding")
+    ]
+    
+    func addSport(name: String) {
+        sports.append(Sport(name: name))
+    }
+    
+    var body: some View {
+        
+        Form {
+            Section {
+                ForEach(sports, id: \.self) {
+                    Text($0.name)
+                }
+                .swipeActions(edge: .leading) {
+                    Button("Edit") {
+                        print("Edit")
+                    }
+                    .tint(.blue)
+                    Button("Delete", role: .destructive) {
+                        print("Delete")
+                    }
+                }
+            }
+        }
+        
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("Sports").font(.headline)
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Add") {
+                        showingAlert = true
+                    }
+                    .alert("Add Sport", isPresented:$showingAlert, actions:
+                            
+                        {
+                        TextField("", text: $addedSport)
+                        Button("Cancel", role: .cancel, action: {})
+                        Button("Add", action: {
+                            addSport(name: addedSport)
+                        })
+                    })
+                    
+                }
+            }
+    }
+}
 
 
--- CrashBuddy/Views/SettingsView.swift
+++ CrashBuddy/Views/SettingsView.swift
@@ -14,98 +14,40 @@ struct Sport: Identifiable, Hashable {
 
 struct SettingsView: View {
 
-    @State private var sport: Sport = Sport(name: "Skiing")
-    @State private var crash_sensitivity: Int = 100
-    @State private var showingAlert = false
-    @State private var addedSport = ""
-
-    @State var sports = [
-        Sport(name: "Skiing"),
-        Sport(name: "Biking"),
-        Sport(name: "Snowboarding")
-    ]
-
-    func addSport(name: String) {
-        sports.append(Sport(name: name))
-    }
-
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    // Debug Mode
-                    NavigationLink(destination: DebugView(), label: {
-                        Text("Debug Mode")
-
-                    }
-                    )
-                    
-                    // Sports
-                    Picker(selection: $sport, label: Text("Sports")) {
-                        // TODO add button here and add logic here
-                        ForEach(sports, id: \.self) {
-                            Text($0.name)
-                        }
-                        .navigationBarTitleDisplayMode(.inline)
-                        .toolbar {
-                            ToolbarItem(placement: .principal) {
-                                VStack {
-                                    Text("Sports").font(.headline)
-                                }
-                            }
-                            
-                            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                                Button("Add") {
-                                    showingAlert = true
-                                }
-                                .alert("Add Sport", isPresented:$showingAlert, actions:
-                                        
-                                        {
-                                    TextField("", text: $addedSport)
-                                    Button("Cancel", role: .cancel, action: {})
-                                    Button("Add", action: {
-                                        addSport(name: addedSport)
-                                    })
-                                })
-                                
-                            }
-                        }
-                        .swipeActions(edge: .leading) {
-                            Button("Edit") {
-                                print("Edit")
-                            }
-                            .tint(.blue)
-                            Button("Delete", role: .destructive) {
-                                print("Delete")
-                            }
-                        }
-                    }
-                    
-                    
-                    // Crash Sensitivity
-                    Picker(selection: $crash_sensitivity, label: Text("Crash Sensitivity")) {
-                        // TODO add button here and add logic here
-                        
-                    }
-                    
-                    // Emergency Contacts
-                    NavigationLink(destination: EmergencyContactsView(), label: {
-                        Text("Emergency Contacts")
-                    }
-                    )
-                    
-                    // "Hardware Connectivity
-                    NavigationLink(destination: HardwareConnectivityView(), label: {
-                        Text("Hardware Connectivity")
-
-                    }
-                    )
-                    .navigationTitle(Text("Settings"))
-                    
-                }
+        Form {
+            Section {
+                // Debug Mode
+                NavigationLink(destination: DebugView(), label: {
+                    Text("Debug Mode")
+                })
+                
+                // Sports
+                NavigationLink(destination: SportsView(), label: {
+                    Text("Sports")
+                })
+                
+                // Crash Sensitivity
+                NavigationLink(destination: CrashSensitivityView(), label: {
+                    Text("Crash Sensitivity")
+                })
+                
+                // Emergency Contacts
+                NavigationLink(destination: EmergencyContactsView(), label: {
+                    Text("Emergency Contacts")
+                })
+                
+                // Hardware Connectivity
+                NavigationLink(destination: HardwareConnectivityView(), label: {
+                    Text("Hardware Connectivity")
+                })
+                .navigationTitle(Text("Settings"))
+                
             }
-            
         }
+        
+        
     }
 }
 
